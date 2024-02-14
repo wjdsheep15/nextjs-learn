5b039918114bd32bc40f7c47be12afcf3d4bd38a
JungSeungGeun <wjd15sheep@gmail.com>
2024-02-14 23:08:57 +0900

    aaa

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import Movie from "../../components/movie";
 import styles from "../../styles/home.module.css";
-import { API_URL } from "../constants";
+import { API_URL } from "../constants.ts";
 
 export const metadata = {
   title: "Home",
